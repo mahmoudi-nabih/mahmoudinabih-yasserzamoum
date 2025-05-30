4abcf8a0d01e40274a7388e8d8ef484b71617fc1
mahmoudi nabih <mahmoudinabih@gmail.com>
2025-05-30 01:00:33 +0100

    first commit

---
+++ contact us .js
@@ -0,0 +1,14 @@
+const form = document.querySelector('.contact-form');
+const inputs = document.querySelectorAll('.form-control');
+
+inputs.forEach(input => {
+    input.addEventListener('focus', () => {
+        input.parentElement.classList.add('focused');
+    });
+
+    input.addEventListener('blur', () => {
+        if (!input.value) {
+            input.parentElement.classList.remove('focused');
+        }
+    });
+});
